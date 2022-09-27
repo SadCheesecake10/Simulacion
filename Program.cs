5eb0032468fa189422bf0a8a40e8ede54650ac56
Roberto Rojas Campos <80497664+SadCheesecake10@users.noreply.github.com>
2022-09-27 18:02:09 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -8,11 +8,15 @@ namespace Simulacion
         public static void Main(string[] args)
         {
             double aux=0;
-            int i=0;
-            double mediaEstadistica=0;
+            int i=0, contador=0;
+            double anchoClase=0.0109;
+            StreamWriter simulacion = new StreamWriter("intervalos.txt");
 
             //Creamos una lista para comparar los numeros del archivo que leeremos
             List<double> listaNumeros = new List<double>();
+            List<List<double>> intervalos= new List<List<double>>();
+            List<double> intervalo= new List<double>();
+            List<int> tamanio= new List<int>();
 
             // Abrimos el archivo, creando un objeto de tipo Workbook para poder obtener su contenido
             Workbook archivo = new Workbook("Numeros Pseudoaleatorios.xlsx");
@@ -28,6 +32,7 @@ namespace Simulacion
 
                 // Guardamos el numero de columnas totales para recorrerlas y obtener su dato.
                 int filas = pagina.Cells.MaxDataRow;
+                int columnas = pagina.Cells.MaxDataColumn;
 
                 // Nos movemos para obtener el dato de cada fila para compararlo.
                 for (  i = 0; i <=filas; i++)
@@ -36,8 +41,6 @@ namespace Simulacion
                     aux=(pagina.Cells[i,2].DoubleValue)/(filas-1);
                     //Agregamos los numeros generados a una lista
                     listaNumeros.Add(aux);
-                    //Vamos sumando los valores para sacar la media estadistica de los datos
-                    mediaEstadistica+=aux;
                 }
                 //Comparamos los numeros de la lista para verificar si tienen relacion -Si tienen relacion detenemos el programa
                 for(int j=0;j<listaNumeros.Count;j++)
@@ -47,14 +50,59 @@ namespace Simulacion
                     {
                         if(aux==listaNumeros[j+1])
                         {
-                            Console.Write("Error los numeros tienen relacion");
+                            Console.WriteLine("Error los numeros tienen relacion");
                             return;
                         } 
-                    }        
+                    }       
                 }
-                mediaEstadistica/=filas;
-                Console.WriteLine("Los "+(i)+" numeros generados son aleatorios. No tienen relacion");
-                Console.WriteLine("La media estadistica es igual "+mediaEstadistica);
+                while(anchoClase<=1)
+                {
+                    contador=1;
+                    for(int j=0;j<listaNumeros.Count;j++)
+                    {
+                        if(listaNumeros[j]>=anchoClase && listaNumeros[j]<(anchoClase+0.0109))
+                        {
+                            intervalo.Add(listaNumeros[j]);
+                            contador++;
+                        }
+                    }
+                    intervalos.Add(intervalo);
+                    intervalo=new List<double>();
+                    tamanio.Add(contador);
+                    anchoClase+=0.0109;
+                }
+                foreach(List<double> lista in intervalos)
+                {
+                    simulacion.WriteLine();
+                    simulacion.Write("Intervalo: ");
+                    foreach(double numero in lista)
+                    {
+                        simulacion.Write(numero+" ");
+                    }
+                    simulacion.WriteLine();
+                    simulacion.WriteLine("Tamaño: "+tamanio[contador]);
+                    simulacion.WriteLine();
+                }
+                simulacion.WriteLine("Los "+(i)+" numeros generados son aleatorios. No tienen relacion");
+                for(int j=0;j<listaNumeros.Count;j++)
+                {
+                    if(listaNumeros[j]<=0.5 && j!=listaNumeros.Count-1)
+                    {
+                        Console.Write("0, ");
+                    }
+                    else if(listaNumeros[j]>0.5 && j!=listaNumeros.Count-1)
+                    {
+                        Console.Write("1, ");
+                    }
+                    else if(listaNumeros[j]<=0.5 && j==listaNumeros.Count-1)
+                    {
+                        Console.Write("0.");
+                    }
+                    else if(listaNumeros[j]>0.5 && j==listaNumeros.Count-1)
+                    {
+                        Console.Write("1.");
+                    }
+                }
             }
         }
     }
